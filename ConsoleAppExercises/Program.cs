83d48c1ee4629c57dfd83d92a8ce787ea52c8bb8
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-06 11:45:10 -0700

    Refactors GetFirstMatchingIndex function

--- ConsoleAppExercises/Program.cs
+++ ConsoleAppExercises/Program.cs
@@ -10,7 +10,27 @@ namespace ConsoleAppExercises
     {
         static void Main(string[] args)
         {
-
+        }
+        public static int GetFirstMatchingIndex(List<int> nums, int valueToMatch) 
+        { 
+            int matchingIndex = -1; 
+ 
+            if (nums == null)
+            {
+                return matchingIndex;
+            }
+            if (nums.Count < 0) 
+            { 
+                return matchingIndex;
+            }
+             for (var i = 0; i < nums.Count; i++) 
+             { 
+                   if (nums[i] == valueToMatch) 
+                    { 
+                       matchingIndex = i; 
+                       return matchingIndex;
+                    } 
+             } 
         }
     }
 }

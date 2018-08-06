94a1f97f871e4f8f4c0875256fb5259a5983219a
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-06 14:01:03 -0700

    Adds c classes

--- ConsoleAppExercises/Program.cs
+++ ConsoleAppExercises/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleAppExercises
@@ -35,15 +37,52 @@ namespace ConsoleAppExercises
         }
         public static void DoStuff() 
         { 
-            Console.WriteLine("Hello Cohort!"); 
-            Thread.Sleep(2000); 
- 
-            var jobTypes = new string[] { "PM", "Dev", "Other" }; 
-            Console.WriteLine("The job types are:"); 
+            const string welcomeCohort = ConfigurationManager.welcomeCohort;
+            const string presentJobTypes = ConfigurationManager.presentJobTypes;
+            const string goodbyeCohort = ConfigurationManager.goodbyeCohort;
+            const int durationOfSleep = ConfigurationManager.durationOfSleep;
+            Console.WriteLine(welcomeCohort); 
+            Thread.Sleep(durationOfSleep); 
+
+            var jobTypes = new string[] { "PM", "Dev", "Other" };
+            Console.WriteLine(presentJobTypes); 
             foreach (var type in jobTypes) 
                 Console.WriteLine(type); 
  
-            Console.WriteLine("Goodbye Cohort!"); 
+            Console.WriteLine(goodbyeCohort); 
         }
+        public class C1 
+        { 
+            public void DoStuff() { } 
+        } 
+        public class C2 
+        { 
+            public void DoStuff() { } 
+        } 
+        public class C3 
+        { 
+            public void DoStuff() { } 
+        } 
+ 
+        public class C4 
+        { 
+            private C1 c1; 
+            private C2 c2; 
+            private C3 c3; 
+ 
+            public C4(C1 c1, C2 c2, C3 c3) 
+            { 
+                this.c1 = c1;
+                this.c2 = c2;
+                this.c3 = c3;
+            } 
+ 
+            public void DoStuff() 
+            { 
+                c1.DoStuff(); 
+                c2.DoStuff(); 
+                c3.DoStuff(); 
+            } 
+        } 
     }
 }
